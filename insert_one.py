086ece58b6f24cb93b13bd255610d4fbf1649830
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-27 21:38:43 +0530

    Modify insert_one.py

--- insert_one.py
+++ insert_one.py
@@ -3,9 +3,9 @@ from pymongo import MongoClient
 
 # install robomongo or robo3T for Graphical User Interface of MongoDB
 
-myClient = MongoClient()
-#client = MongoClient("localhost", 27017)             *Alternate CODE in comments*
-#client = MongoClient("mongodb://localhost:27017/")
+#myClient = MongoClient()
+myClient = MongoClient("localhost", 27017)             #Alternate CODE in comments
+#myClient = MongoClient("mongodb://localhost:27017/")
 db = myClient.mydb
 #db = myClient["my-database"]
 
