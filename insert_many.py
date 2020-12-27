19dc695dde49c4bc44758b9ed3ca94cf3d7c35cc
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-27 21:38:26 +0530

    Modify insert_many.py

--- insert_many.py
+++ insert_many.py
@@ -9,3 +9,10 @@ myClient = MongoClient()
 db = myClient.mydb
 #db = myClient["my-database"]
 
+users = [{"username":"first", "password":"secure"}, {"username":"abhishek","password":"verysecure"}]
+
+Users = db.users
+
+inserted = Users.insert_many(users)
+
+print(inserted.inserted_ids)
