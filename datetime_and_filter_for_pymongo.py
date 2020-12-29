efada3aae8b5d3d2de10ee5364f198e4064d3c01
abhishekchandra2522k <1ac23456789@gmail.com>
2020-12-29 22:16:54 +0530

    Add datetime_and_filter_for_pymongo.py

---
+++ datetime_and_filter_for_pymongo.py
@@ -0,0 +1,23 @@
+import datetime
+import pymongo
+from pymongo import MongoClient
+
+myClient = MongoClient()
+
+db = myClient.mydb
+
+Users = db.users
+
+current_date = datetime.datetime.now()
+
+#print(current_date)
+
+old_date = datetime.datetime(2009,12,29)
+
+#uid = Users.insert_one({"username":"ram", "date":current_date})
+
+# $gt : greater than,  $lt : less than, $lte : less than equal to, $ne : not equal to, $exists : True or False
+print(Users.count_documents(filter={"date":{"$gt":old_date}}))
+
+print(Users.count_documents(filter={"date" : {"$exists" : True}}))
+print(Users.count_documents(filter={"username" : {"$ne" : "abhishek"}}))
